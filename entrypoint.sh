dc23b203c4f88a5fd35c265c5c0758e88cf3b9e7
Inbisat Naveed <inkyn2002@gmail.com>
2026-03-01 18:01:19 +0100

    fix: update entrypoint for permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,16 @@ if [ -z "$APP_KEY" ]; then
     php artisan key:generate --force
 fi
 
-php artisan migrate:fresh --seed --force
+chmod -R 775 /var/www/storage
+
+php artisan migrate --force
+
+if [ ! -f /var/www/storage/oauth-private.key ]; then
+    php artisan passport:install --force
+fi
+
+chmod 600 /var/www/storage/oauth-private.key
+chmod 644 /var/www/storage/oauth-public.key
 
 echo "App ready."
 
