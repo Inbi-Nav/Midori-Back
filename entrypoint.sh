915a2c6f070f953b416f7cc21a22016560b79eed
Inbisat Naveed <inkyn2002@gmail.com>
2026-03-01 13:49:33 +0100

    feat: add passport install in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,10 @@ fi
 
 php artisan migrate --force
 
+if [ ! -f storage/oauth-private.key ]; then
+    php artisan passport:install --force
+fi
+
 echo "Aplicación lista."
 
 exec php artisan serve --host=0.0.0.0 --port=${PORT:-8080}
